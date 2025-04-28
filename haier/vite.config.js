eb64eaee6cbc65499634409cea4d3b9e39fd8422
King-barry-coder <ajibolaisrael1@gmail.com>
2025-04-28 15:27:43 +0100

    correction of base

--- haier/vite.config.js
+++ haier/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/HaierStore/", 
+  base: "/", 
 });
 
 // import { defineConfig } from 'vite'
